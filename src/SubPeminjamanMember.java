534e4bc8e63b932f5d1267b9832df532a6f1ed1b
Jidan0410 <hizkiaazidan04@gmail.com>
2021-06-22 09:56:43 +0800

    7. Membuat Getter and Setter untuk seluruh atribut pada class no. 2

--- src/SubPeminjamanMember.java
+++ src/SubPeminjamanMember.java
@@ -25,4 +25,60 @@ public class SubPeminjamanMember extends PeminjamanMember {
         System.out.println("No Telepon Peminjam :"+no_telp);
         System.out.println("Alamat Peminjam :"+alamat);
     }
+
+    public int getKode_buku() {
+        return kode_buku;
+    }
+
+    public void setKode_buku(int kode_buku) {
+        this.kode_buku = kode_buku;
+    }
+
+    public int getKode_peminjam() {
+        return kode_peminjam;
+    }
+
+    public void setKode_peminjam(int kode_peminjam) {
+        this.kode_peminjam = kode_peminjam;
+    }
+
+    public Date getTgl_pinjam() {
+        return tgl_pinjam;
+    }
+
+    public void setTgl_pinjam(Date tgl_pinjam) {
+        this.tgl_pinjam = tgl_pinjam;
+    }
+
+    public Date getTgl_pengembalian() {
+        return tgl_pengembalian;
+    }
+
+    public void setTgl_pengembalian(Date tgl_pengembalian) {
+        this.tgl_pengembalian = tgl_pengembalian;
+    }
+
+    public String getNamapeminjam() {
+        return namapeminjam;
+    }
+
+    public void setNamapeminjam(String namapeminjam) {
+        this.namapeminjam = namapeminjam;
+    }
+
+    public String getNo_telp() {
+        return no_telp;
+    }
+
+    public void setNo_telp(String no_telp) {
+        this.no_telp = no_telp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
 }
