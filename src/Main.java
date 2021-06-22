a8aeacd1c651349da90533778d5979b961a93926
Jidan0410 <hizkiaazidan04@gmail.com>
2021-06-22 09:55:00 +0800

    5. Membuat 2 (dua) object untuk tiap constructor class no. 1, sehingga total ada 6 (enam) object class no. 1 pada class no. 3

--- src/Main.java
+++ src/Main.java
@@ -32,5 +32,33 @@ public class Main {
         peminjambsa1.meminjambukuberjangkawaktu();
         peminjambsa1.mengembalikanbukuberjangkawaktu();
 
+        //Construc1
+        PeminjamanMember a1_objct = new PeminjamanMember(02,19710002,2,20000,002,"Ari","Huja","Ari Hujan", new Date(), new Date());
+        a1_objct.tampilkaninfopeminjam();
+        a1_objct.meminjambuku();
+        a1_objct.mengembalikanbuku();
+        a1_objct.membelibuku();
+
+        PeminjamanMember a2_objct = new PeminjamanMember(03,19710003,3,20000,003,"Aris","Hujar","Ari Hujan Basah", new Date(), new Date());
+        a2_objct.tampilkaninfopeminjam();
+        a2_objct.meminjambuku();
+        a2_objct.mengembalikanbuku();
+        a2_objct.membelibuku();
+
+        //Construc2
+        PeminjamanMember a3_objct = new PeminjamanMember(04,19710004,4,20000,004,"Arif","Hujaf","Ari Hujan Dingin");
+        a3_objct.meminjambuku();
+        a3_objct.mengembalikanbuku();
+
+        PeminjamanMember a4_objct = new PeminjamanMember(05,19710003,5,20000,005,"Ariv","Hujag","Ari Hujan Deras");
+        a4_objct.meminjambuku();
+        a4_objct.mengembalikanbuku();
+
+        //Construc3
+        PeminjamanMember a5_objct = new PeminjamanMember(001,19710001,01,"Haris");
+        a5_objct.membelibuku();
+
+        PeminjamanMember a6_objct = new PeminjamanMember(002,19710002,02,"Haris");
+        a6_objct.membelibuku();
     }
 }
