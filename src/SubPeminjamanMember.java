07dad4a5d8f0cbf1df9376bc9a2587588c20bcc6
Jidan0410 <hizkiaazidan04@gmail.com>
2021-06-22 09:03:18 +0800

    2. Membuat subclass dari class no. 1 tambahkan 3 (tiga) atribut dan 2 (dua) method (selain getter, setter, dan CRUD)

---
+++ src/SubPeminjamanMember.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class SubPeminjamanMember {
+    int kode_buku;
+    int kode_peminjam;
+    Date tgl_pinjam;
+    Date tgl_pengembalian;
+    String namapeminjam;
+    String no_telp;
+    String alamat;
+
+    public void meminjambukuberjangkawaktu(){
+        System.out.println("Kode Buku Dipinjam :"+kode_buku);
+        System.out.println("Tanggal Meminjam :"+tgl_pinjam);
+        System.out.println("Kode Peminjam Biasa :"+kode_peminjam);
+        System.out.println("Nama Peminjam :"+namapeminjam);
+        System.out.println("No Telepon Peminjam :"+no_telp);
+        System.out.println("Alamat Peminjam :"+alamat);
+    }
+    public void mengembalikanbukuberjangkawaktu(){
+        System.out.println("Kode Buku Dipinjam :"+kode_buku);
+        System.out.println("Tanggal Mengembalikan :"+tgl_pengembalian);
+        System.out.println("Kode Peminjam Biasa :"+kode_peminjam);
+        System.out.println("Nama Peminjam :"+namapeminjam);
+        System.out.println("No Telepon Peminjam :"+no_telp);
+        System.out.println("Alamat Peminjam :"+alamat);
+    }
+}
