765ee916bc7ebad2832809313f45740aa65bd97b
Jidan0410 <hizkiaazidan04@gmail.com>
2021-06-22 09:20:33 +0800

    3. Membuat sebuah class untuk menciptakan object dari soal no. 1 dan no. 2

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        PeminjamanMember peminjam1 = new PeminjamanMember();
+        peminjam1.id = 01;
+        peminjam1.nim = 19710001;
+        peminjam1.jumlahbuku = 2;
+        peminjam1.harga = 20000;
+        peminjam1.id_buku = 001;
+        peminjam1.nama = "Yanti";
+        peminjam1.pegawai = "Asep";
+        peminjam1.nama_buku = "Laskar";
+        peminjam1.pinjaman = new Date();
+        peminjam1.pengembalian = new Date();
+
+        SubPeminjamanMember peminjambsa1 = new SubPeminjamanMember();
+        peminjambsa1.kode_buku = 01;
+        peminjambsa1.kode_peminjam = 001;
+        peminjambsa1.tgl_pinjam = new Date();
+        peminjambsa1.tgl_pengembalian = new Date();
+        peminjambsa1.namapeminjam = "Saha";
+        peminjambsa1.no_telp = "081224234576";
+        peminjambsa1.alamat = "Amuntai";
+
+    }
+}
